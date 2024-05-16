e757b2a7c3c1c6be69701de22baae55cf43ed02e
Briony Frater <briefrater@gmail.com>
2024-05-16 16:19:10 -0500

    not fouc

--- index.js
+++ index.js
@@ -1,27 +1,5 @@
 $(document).ready(function () {
-    
-    
-    tailwind.config = {
-        theme: {
-          extend: {
-            colors: {
-              'yellow': '#ebff00',
-              'green': '#d4ff3f',
-              'white': '#fefefe'
-            },
-            
-            fontFamily: {
-              'sans': ['"Montserrat"', 'sans-serif'],
-              'mono': ['"JetBrains Mono"', 'monospace']
-            },
-
-            // screens:{
-            //   'sm': {'min': '600px', 'max': '767px'}
-            // }
-          }
-        }
-      }
-
+  
       lucide.createIcons();
     
 });
