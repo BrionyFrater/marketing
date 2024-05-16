e9d7033bee0daabb131919bdf082ca936ba218b9
BrionyFrater <briefrater@gmail.com>
2024-05-16 02:05:35 -0500

    testing some styles

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
     lucide.createIcons();
     
-    
+    tailwind.config = {
+        theme: {
+          extend: {
+            colors: {
+              clifford: '#da373d',
+              yellow: '#ebff00',
+              green: '#d4ff3f'
+            }
+          }
+        }
+      }
 });
