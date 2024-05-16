f34e21e3bdc8821eca2933f08fd7a627c440d5a7
Briony Frater <briefrater@gmail.com>
2024-05-16 03:25:48 -0500

    added config

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          yellow: '#ebff00',
+          green: '#d4ff3f'
+        }
+      }
+    }
+  }
