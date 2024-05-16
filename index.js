8579a19740d8676dca75f06da4c534a707f7bc96
Briony Frater <briefrater@gmail.com>
2024-05-16 04:49:33 -0500

    .

--- index.js
+++ index.js
@@ -2,15 +2,5 @@ $(document).ready(function () {
     lucide.createIcons();
     
 
-    tailwind.config = {
-        theme: {
-          extend: {
-            colors: {
-              clifford: '#da373d',
-              yellow: '#ebff00',
-              green: '#d4ff3f'
-            }
-          }
-        }
-    }
+    
 });
